7b4f0a536b2992a0c3b6ff01f31b00ede2aedaf9
TheElders1 <basseysamuel2023@gmail.com>
2025-12-04 11:27:31 +0100

    Updated RegisterPage.tsx

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -248,7 +248,7 @@ export default function RegisterPage() {
 
                 <div>
                   <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-                    Middle Name
+                    Middle Name (optional)
                   </label>
                   <input
                     type="text"
